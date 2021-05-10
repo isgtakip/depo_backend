992caef4458c8aa60afebe2fcf37e193929b91a7
Okan SARI <sari.okan@gmail.com>
2021-05-10 22:33:58 +0300

    dfsafsdfsadf

--- app/Models/Malzemeler.php
+++ app/Models/Malzemeler.php
@@ -11,7 +11,7 @@ class Malzemeler extends Model
     protected $primaryKey = 'malzeme_id';
     public $timestamps = false;
     protected $casts = [ 'malzeme_miktar' => 'integer'];
-    protected $casts = [ 'miktar' => 'integer'];
+
 
     protected $fillable = [
         'malzeme_adi',
