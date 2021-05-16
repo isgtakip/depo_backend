c0857206d241db77c9750e420df8f27e61f451c9
Okan SARI <sari.okan@gmail.com>
2021-05-16 23:41:35 +0300

    bakalım

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/public/*', 'sanctum/csrf-cookie','login','logout'],
 
     'allowed_methods' => ['*'],
 
