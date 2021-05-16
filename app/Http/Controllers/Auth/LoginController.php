a0de15649a53f7394ec543c9041f55dc94062fb8
Okan SARI <sari.okan@gmail.com>
2021-05-16 14:08:25 +0300

    kjhkjhk

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -21,7 +21,7 @@ class LoginController extends Controller
             
         }
 
-    $request->session()->regenerate();
+   // $request->session()->regenerate();
 
     return  response()-json(null,201)
 
