928442ef8b1eba292a303a0795f5b2f0abbe475e
Okan SARI <sari.okan@gmail.com>
2021-05-16 19:22:40 +0300

    adsad

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,8 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
+
 
     /**
      * The attributes that are mass assignable.
