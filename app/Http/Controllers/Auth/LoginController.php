edfd770224025e599b4b214345dffe5118bc9d4b
Okan SARI <sari.okan@gmail.com>
2021-05-16 05:08:11 +0300

    ewrer

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\ValidationException;
+
 
 
 
@@ -14,7 +15,9 @@ class LoginController extends Controller
     public function __invoke(Request $request){
 
         if (!auth()->attempt($request->only('email','password'))){
-                throw new AuthenticationExpection();
+            throw ValidationException::withMessages([
+                'email' => ['Credentials are incorrect.'],
+            ]);
             
         }
 
