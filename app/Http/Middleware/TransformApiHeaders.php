701cc760fbe6ada459344cc35b0364ff75019fcc
Okan SARI <sari.okan@gmail.com>
2021-05-21 00:27:27 +0300

    son şans

--- app/Http/Middleware/TransformApiHeaders.php
+++ app/Http/Middleware/TransformApiHeaders.php
@@ -4,24 +4,17 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+class TransformApiHeaders  { 
 
-class TransformApiHeaders
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle(Request $request, Closure $next)
-    {
-        $cookie_name='XSRF-TOKEN';
-        $token_cookie=$request->cookie($cookie_name);
-        
-        if ($token_cookie!==null) {
-            $request->headers()->add(["X-$cookie_name"=>$token_cookie]);
+    public function handle($request, Closure $next) {
+
+        $cookie_name = 'XSRF-TOKEN'; 
+        $token_cookie = $request->cookie($cookie_name); 
+
+        if ($token_cookie !== null) { 
+            $request->headers->add(["X-$cookie_name" => $token_cookie]); 
         }
-        return $next($request);
+
+        return $next($request); 
     }
 }
