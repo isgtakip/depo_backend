6692f9bd4054cfcd298d0cc5b281e12e1e923cab
Okan SARI <sari.okan@gmail.com>
2021-05-20 22:49:35 +0300

    haydi bastır

---
+++ app/Http/Middleware/TransformApiHeaders.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class TransformApiHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $cookie_name='XSRF-TOKEN';
+        $token_cookie=$request->cookie($cookie_name);
+        if ($token_cookie!==null) {
+            $request->headers()->add(["X-$cookie_name"=>$token_cookie]);
+        }
+        return $next($request);
+    }
+}
